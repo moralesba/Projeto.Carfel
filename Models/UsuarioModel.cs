7119417ed507ad72b9c9aee77cba7b5000690ff4
Fábio de Lapaz <ba.morales@hotmail.com>
2018-12-05 22:57:10 -0200

    atualizado

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Projeto.Carfel.Comentarios.Models {
+    public class UsuarioModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Projeto.Carfel.Comentarios
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(
+                options => options.IdleTimeout = TimeSpan.FromMinutes(30)
+            );
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSession();
+
+            app.UseStaticFiles();
+
+            app.UseMvc( 
+                rota => rota.MapRoute(
+                    name: "defaults",                     
+                    template: "{controller=Usuario}/{action=masterpage}")  
+            );
+        }
+    }
+}
