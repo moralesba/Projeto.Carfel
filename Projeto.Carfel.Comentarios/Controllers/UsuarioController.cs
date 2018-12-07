0d12a0172369e9a52eea3daf36ae56f9451dda47
Barbara Morales de Lapaz <47015374897@info.local>
2018-12-07 07:51:42 -0200

    atualizado

---
+++ Projeto.Carfel.Comentarios/Controllers/UsuarioController.cs
@@ -0,0 +1,150 @@
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto.Carfel.Comentarios.Models;
+using Projeto.Carfel.Comentarios.Repositorios;
+
+namespace Projeto.Carfel.Comentarios.Controllers {
+    public class UsuarioController : Controller{
+        
+        [HttpGet]
+        public ActionResult MasterPage (){
+            return View ();
+        }
+        public IActionResult Cadastro () {
+            return RedirectToAction("MasterPage");
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(IFormCollection form) {
+            UsuarioModel usuario = new UsuarioModel();
+            UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio();
+            
+            string[] UsuariosSalvos;
+            int count=0;
+            string nome, email, senha;
+            
+            if (System.IO.File.Exists("usuarios.csv")) {
+                UsuariosSalvos = System.IO.File.ReadAllLines("usuarios.csv");
+
+                for (int i=0; i< UsuariosSalvos.Length; i++) {
+                    if (!UsuariosSalvos[i].Contains("admin@carfel.com")) {
+                        count++;           
+                    }
+                }
+
+                if (count == UsuariosSalvos.Length) {
+                    usuario.Email = "admin@carfel.com";
+                    usuario.Nome = "Administrador";
+                    usuario.Senha="admincarfel";
+                    usuario.Tipo="admin";
+                    usuario.Id =1;
+
+                    usuarioRepositorio.Cadastrar(usuario);
+                }
+             }
+            
+            count=0;
+            if (!string.IsNullOrEmpty(form["nome"])) {
+                nome =form["nome"];
+                if (nome.Length>=3) {
+                    usuario.Nome = form["nome"];
+                }
+            }
+            else {
+                TempData["MensagemCadastro"] = "Nome obrigatório";
+                return RedirectToAction("MasterPage");
+            }
+
+             if (System.IO.File.Exists("usuarios.csv")) {
+                UsuariosSalvos = System.IO.File.ReadAllLines("usuarios.csv");
+
+                
+                for (int i=0; i< UsuariosSalvos.Length; i++) {
+                    if (!UsuariosSalvos[i].Contains(form["email"])) {
+                        count++;           
+                    }
+                }
+
+                if (count == UsuariosSalvos.Length || count==0) {
+                    if (!string.IsNullOrEmpty(form["email"])){
+                        email = form["email"];
+                        if (email.Contains("@") && email.Contains(".") && email.Length>=10) {
+                            usuario.Email = form["email"];
+                        }
+                    }
+                    else {
+                        TempData["MensagemCadastro"] = "Email inválido";
+                        return RedirectToAction("MasterPage");
+                    }
+                }
+
+                else {
+                    TempData["MensagemCadastro"] = "Email ja cadastrado";
+                    return RedirectToAction("MasterPage");
+                }
+             }
+            
+            if (!string.IsNullOrEmpty(form["senha"])){
+                senha = form["senha"];
+                if (senha.Length>=6) 
+                    usuario.Senha = form["senha"];
+            }
+            else {
+                TempData["MensagemCadastro"] = "Senha inválida (min 6 caracteres)";
+                return RedirectToAction("MasterPage");
+            }
+
+            usuario.Tipo = "comum";
+            
+            if (System.IO.File.Exists("usuarios.csv")) {
+                UsuariosSalvos = System.IO.File.ReadAllLines("usuarios.csv");
+
+                usuario.Id = UsuariosSalvos.Length + 1;
+            }
+
+            usuarioRepositorio.Cadastrar(usuario);
+            TempData["MensagemCadastro"] = "Usuário cadastrado";
+            return RedirectToAction("MasterPage");
+        }
+
+        [HttpGet]
+        public IActionResult Login() {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult Login(IFormCollection form) {
+            UsuarioRepositorio usuarioRepositorio = new UsuarioRepositorio();
+            UsuarioModel usuario = usuarioRepositorio.Login(form["email"], form["senha"]);
+
+            if  (System.IO.File.Exists("usuarios.csv")) {
+
+                if (usuario!=null){
+
+                    if (usuario.Email=="admin@carfel.com") {
+                        HttpContext.Session.SetString("emailUsuario", usuario.Email); 
+                        HttpContext.Session.SetString("nomeUsuario",usuario.Nome);
+
+                        TempData["UsuarioLogado"] = usuario.Nome;
+                        
+                        return RedirectToAction("Aprovacao", "Comentario");
+                    }
+
+                    else {
+                        HttpContext.Session.SetString("emailUsuario", usuario.Email);
+                        HttpContext.Session.SetString("nomeUsuario",usuario.Nome);
+
+                        TempData["UsuarioLogado"] = usuario.Nome;
+                        
+
+                        return RedirectToAction("MasterPage");
+                    }
+                }
+            }
+            TempData["MensagemLogin"]= "Usuário/senha inválido";
+            return  RedirectToAction("MasterPage");
+        }
+    }
+}
---
+++ Projeto.Carfel.Comentarios/Repositorios/UsuarioReporitorioCSV.cs
@@ -0,0 +1,112 @@
+using System.Collections.Generic;
+using System.IO;
+using Projeto.Carfel.Comentarios.Interfaces;
+using Projeto.Carfel.Comentarios.Models;
+
+namespace Projeto.Carfel.Comentarios.Repositorios {
+    public class UsuarioReporitorioCSV : IUsuario {
+        public UsuarioModel BuscarPorEmailESenha (string email, string senha) {
+            List<UsuarioModel> usuariosCadastrados = CarregarDoCSV ();
+
+            foreach (UsuarioModel usuario in usuariosCadastrados) {
+                if (usuario.Email == email && usuario.Senha == senha) {
+                    return usuario;
+                }
+            }
+            return null;
+        }
+        public List<UsuarioModel> Listar () => CarregarDoCSV ();
+
+        private List<UsuarioModel> CarregarDoCSV () {
+            List<UsuarioModel> lsUsuarios = new List<UsuarioModel> ();
+
+            string[] linhas = File.ReadAllLines ("usuarios.csv");
+
+            foreach (string linha in linhas) {
+                if (string.IsNullOrEmpty (linha)) {
+                    continue;
+                }
+
+                string[] dadosDaLinha = linha.Split (';');
+
+                UsuarioModel usuario = new UsuarioModel (
+                    id: int.Parse (dadosDaLinha[0]),
+                    nome: dadosDaLinha[1],
+                    email: dadosDaLinha[2],
+                    senha: dadosDaLinha[3]
+                );
+
+                lsUsuarios.Add (usuario);
+            }
+            return lsUsuarios;
+        }
+        public void Excluir (int id) {
+            string[] linhas = File.ReadAllLines ("usuarios.csv");
+
+            for (int i = 0; i < linhas.Length; i++) {
+                string[] dadosDaLinha = linhas[i].Split (';');
+
+                if (id.ToString () == dadosDaLinha[0]) {
+                    linhas[i] = "";
+                    break;
+                }
+            }
+            File.WriteAllLines ("usuarios.csv", linhas);
+        }
+
+        public UsuarioModel Cadastrar (UsuarioModel usuario) {
+            if (File.Exists ("usuarios.csv")) {
+                usuario.ID = System.IO.File.ReadAllLines ("usuarios.csv").Length + 1;
+            } else {
+                usuario.ID = 1;
+            }
+            using (StreamWriter sw = new StreamWriter ("usuarios.csv", true)) {
+                sw.WriteLine ($"{usuario.ID};{usuario.Nome};{usuario.Email};{usuario.Senha}");
+            }
+            return usuario;
+        }
+
+        public UsuarioModel Editar (UsuarioModel usuario) {
+            string[] linhas = System.IO.File.ReadAllLines ("usuarios.csv");
+
+            for (int i = 0; i < linhas.Length; i++) {
+                if (string.IsNullOrEmpty (linhas[i])) {
+                    continue;
+                }
+
+                string[] dados = linhas[i].Split (";");
+
+                if (dados[0] == usuario.ID.ToString ()) {
+                    linhas[i] = $"{usuario.ID};{usuario.Nome};{usuario.Email};{usuario.Senha}";
+                    break;
+                }
+            }
+
+            System.IO.File.WriteAllLines ("usuarios.csv", linhas);
+            return usuario;
+        }
+
+        public UsuarioModel BuscarPorId (int id) {
+            string[] linhas = System.IO.File.ReadAllLines ("usuarios.csv");
+
+            for (int i = 0; i < linhas.Length; i++) {
+                if (string.IsNullOrEmpty (linhas[i])) {
+                    continue;
+                }
+
+                string[] dados = linhas[i].Split (';');
+
+                if (dados[0] == id.ToString ()) {
+                    UsuarioModel usuario = new UsuarioModel (
+                        id: int.Parse (dados[0]),
+                        nome: dados[1],
+                        email: dados[2],
+                        senha: dados[3]
+                    );
+                    return usuario;
+                }
+            }
+            return null;
+        }
+    }
+}
